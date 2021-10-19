d07f300b8dccdcdf8adce51e88dd794a3a8d4c1b
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-10-19 10:56:00 +0530

    Update gsddb.py

--- gsddb.py
+++ gsddb.py
@@ -1,8 +1,8 @@
 #https://dev.mysql.com/doc/mysql-installation-excerpt/5.6/en/docker-mysql-getting-started.html
 import mysql.connector
 
-rm_host = 'localhost'
-#rm_host = '192.168.0.25:3306'
+#rm_host = 'localhost'
+rm_host = '192.168.0.28:3306'
 
 mydb = mysql.connector.connect(
   host=rm_host,
