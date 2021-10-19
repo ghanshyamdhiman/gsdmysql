f8fa540d8ec41fa27c2da5ebed66a954603e28fd
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-10-19 19:22:58 +0530

    Update gsddb.py

--- gsddb.py
+++ gsddb.py
@@ -1,4 +1,5 @@
 #https://dev.mysql.com/doc/mysql-installation-excerpt/5.6/en/docker-mysql-getting-started.html
+#https://bobcares.com/blog/edit-docker-image/
 import mysql.connector
 
 #rm_host = 'localhost'
@@ -7,7 +8,7 @@ rm_host = '192.168.0.28'
 mydb = mysql.connector.connect(
   host=rm_host,
   user="root",
-  password="Gsd@12345",
+  password="root123",
   database="testdb1"
 )
 
