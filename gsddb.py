aabd0acb5473811663ca7cf8fa76b8d46574bcc2
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-10-20 00:17:45 +0530

    Update gsddb.py

--- gsddb.py
+++ gsddb.py
@@ -1,7 +1,8 @@
 #https://dev.mysql.com/doc/mysql-installation-excerpt/5.6/en/docker-mysql-getting-started.html
 #https://bobcares.com/blog/edit-docker-image/
 #https://www.macstadium.com/blog/how-to-k8s-pull-edit-and-push-a-docker-image
-d#ocker run -p 5432:5432 -it postgres
+#docker run -p 5432:5432 -it postgres
+#https://www.sqlshack.com/how-to-connect-to-remote-mysql-server-using-ssl-on-ubuntu-18-04/
 import mysql.connector
 
 #rm_host = 'localhost'
