49f2a9d530bdcd582ff8f2154272a4d2d8dc5e7f
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-10-19 10:48:21 +0530

    Create gsddb.py

---
+++ gsddb.py
@@ -0,0 +1,21 @@
+#https://dev.mysql.com/doc/mysql-installation-excerpt/5.6/en/docker-mysql-getting-started.html
+import mysql.connector
+
+rm_host = 'localhost'
+#rm_host = '192.168.0.25:3306'
+
+mydb = mysql.connector.connect(
+  host=rm_host,
+  user="root",
+  password="Gsd@12345",
+  database="testdb1"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM testtbl1")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
